d164e522b1b494a97530d03dfaff33e6fce4d067
3443e <3443p@proton.me>
2026-06-01 03:47:29 +0100

    fixed time pthread thing in logger i think

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,18 +1,13 @@
-#include <chrono>
+//#include <chrono>
 #include <iomanip> // for std::put_time()
 #include <iostream>
 #include "Logger.hpp"
 
 std::string Logger::GetTime() {
-    auto now = std::chrono::system_clock::now();
-    std::time_t t = std::chrono::system_clock::to_time_t(now);
+    std::time_t t = std::time(nullptr);
 
     std::tm tm{};
-#ifdef _WIN32
-    localtime_s(&tm, &t);
-#else
     localtime_r(&t, &tm);
-#endif
 
     std::ostringstream oss;
     oss << std::put_time(&tm, "%H:%M:%S");
